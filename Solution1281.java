a1b005f84d7b93debc0ddedb7e73cad3f05ac132
Konduru <harshitha.konduru@sap.com>
2024-03-08 23:46:00 +0530

    1281 solution added

---
+++ Solution1281.java
@@ -0,0 +1,27 @@
+public class Solution1281 {
+    public int subtractProductAndSum(int n) {
+        //Space Complexity - O(1)
+        //Time Complexity - O(N) - Suppose there are N digits in the number n
+        
+        //Initialize the product to 1 and sum to 0
+        int product = 1;
+        int sum = 0;
+
+        //Compute the product and sum of its digits
+        while(n > 0) {
+            //Fetch the last digit
+            int lastDigit = n % 10;
+
+            //Multiply the digit with the product
+            product = product * lastDigit;
+            //Add the digit to the sum
+            sum = sum + lastDigit;
+            
+            //Remove the last digit from the number
+            n = n / 10;
+        }
+
+        //Return the difference between product and sum
+        return product - sum;
+    }
+}
